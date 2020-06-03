28f193aee1a0c738c184de744b196bb3f093ba75
Dinezhkumar <47935348+Dinezhkumar@users.noreply.github.com>
2020-06-03 23:10:15 +0530

    save logics

--- EduSite/EduSite/Controllers/HomeController.cs
+++ EduSite/EduSite/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using static UnivSite.Models.StudentDetails;
+using EduSite.SqlDal;
+using System.Web.WebPages;
 
 namespace EduSite.Controllers
 {
@@ -15,16 +17,108 @@ namespace EduSite.Controllers
             return View(studentDetailsModel);
         }
 
-        public ActionResult SavePersonalDetails()
+        [HttpPost]
+        public ActionResult SavePersonalDetails(FormCollection formCollection)
         {
+            PersonalDetails personalDetails = new PersonalDetails();
+
+            if(!string.IsNullOrEmpty(formCollection["Name"]))
+            {
+                personalDetails.ApplicantName = formCollection["Name"].ToString();
+            }
+            if(!string.IsNullOrEmpty(formCollection["MotherName"]))
+            {
+                personalDetails.MotherName = formCollection["MotherName"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["FatherName"]))
+            {
+                personalDetails.MotherName = formCollection["FatherName"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["RegistrationNo"]))
+            {
+                personalDetails.RegistrationNo = formCollection["RegistrationNo"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["NatureOfDocument"]))
+            {
+                personalDetails.RegistrationNo = formCollection["NatureOfDocument"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["ReasonOfApplying"]))
+            {
+                personalDetails.RegistrationNo = formCollection["ReasonOfApplying"].ToString();
+            }
+            StudentDetailsDal studentDetailsDal = new StudentDetailsDal();
+            studentDetailsDal.AddPersonalDetails(personalDetails);
+            return View("PersonalDetails.cshtml", personalDetails);
+        }
+
+        [HttpPost]
+        public ActionResult SaveAddressDetails(FormCollection formCollection)
+        {
+            AddressDetails addressDetails = new AddressDetails();
+            if (!string.IsNullOrEmpty(formCollection["FullAddress"]))
+            {
+                addressDetails.FullAddress = formCollection["FullAddress"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["PhoneNumberOffice"]))
+            {
+                addressDetails.PhoneNumberOffice = formCollection["PhoneNumberOffice"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["PhoneNumberResidence"]))
+            {
+                addressDetails.PhoneNumberResidence = formCollection["PhoneNumberResidence"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["EmailId"]))
+            {
+                addressDetails.EmailId = formCollection["EmailId"].ToString();
+            }
+            StudentDetailsDal studentDetailsDal = new StudentDetailsDal();
+            studentDetailsDal.SaveAddressDetails(addressDetails);
             return View();
         }
-        public ActionResult SaveAddressDetails()
+
+        [HttpPost]
+        public ActionResult SaveFeeDetails(FormCollection formCollection)
         {
-            return View();
-        }
-        public ActionResult SaveFeeDetails()
-        {
+            FeeDetails feeDetails = new FeeDetails();
+
+            if (!string.IsNullOrEmpty(formCollection["StudentId"]))
+            {
+                feeDetails.StudentId = formCollection["StudentId"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["TransactionId"]))
+            {
+                feeDetails.TransactionId = formCollection["TransactionId"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["Date"]))
+            {
+                feeDetails.Date = formCollection["Date"].AsDateTime();
+            }
+            if (!string.IsNullOrEmpty(formCollection["PayeeName"]))
+            {
+                feeDetails.PayeeName = formCollection["PayeeName"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["FathersName"]))
+            {
+                feeDetails.FathersName = formCollection["FathersName"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["DOB"]))
+            {
+                feeDetails.DOB = formCollection["DOB"].AsDateTime();
+            }
+            if (!string.IsNullOrEmpty(formCollection["PhoneNumberOffice"]))
+            {
+                feeDetails.PhoneNumberOffice = formCollection["PhoneNumberOffice"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["FeeDescription"]))
+            {
+                feeDetails.FeeDescription = formCollection["FeeDescription"].ToString();
+            }
+            if (!string.IsNullOrEmpty(formCollection["Amount"]))
+            {
+                feeDetails.Amount = Convert.ToDecimal(formCollection["Amount"]);
+            }
+            StudentDetailsDal studentDetailsDal = new StudentDetailsDal();
+            studentDetailsDal.AddFeeDetails(feeDetails);
             return View();
         }
 
---
+++ EduSite/EduSite/SqlDal/StudentDetailsDal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using static UnivSite.Models.StudentDetails;
+
+namespace EduSite.SqlDal
+{
+    public class StudentDetailsDal
+    {
+        public void AddPersonalDetails(PersonalDetails personalDetails)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.
+             ConnectionStrings["connectionStringName"].ConnectionString;
+            using(SqlConnection connection=new SqlConnection(connectionString))
+            {
+                string query = "insert into PersonalDetails(ApplicantName,FatherName,MotherName,RegistrationNo,NatureOfDocument,ReasonOfApplying)values(@ApplicantName,@FatherName,@MotherName,@RegistrationNo,@NatureOfDocument,@ReasonOfApplying)";
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+                sqlCommand.Parameters.Add("@ApplicantName", System.Data.SqlDbType.NVarChar).Value = personalDetails.ApplicantName;
+                sqlCommand.Parameters.Add("@FatherName", System.Data.SqlDbType.NVarChar).Value = personalDetails.FatherName;
+                sqlCommand.Parameters.Add("@MotherName", System.Data.SqlDbType.NVarChar).Value = personalDetails.MotherName;
+                sqlCommand.Parameters.Add("@RegistrationNo", System.Data.SqlDbType.NVarChar).Value = personalDetails.RegistrationNo;
+                sqlCommand.Parameters.Add("@NatureOfDocument", System.Data.SqlDbType.NVarChar).Value = personalDetails.NatureOfDocument;
+                sqlCommand.Parameters.Add("@ReasonOfApplying", System.Data.SqlDbType.NVarChar).Value = personalDetails.ReasonOfApplying;
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                
+            }
+           
+        }
+        public void AddFeeDetails(FeeDetails feeDetails)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.
+             ConnectionStrings["connectionStringName"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "insert into FeeDetails(TransactionId,Date,PayeeName,FathersName,DOB,PhoneNumberOffice,FeeDescription)values(@TransactionId,@Date,@PayeeName,@FathersName,@DOB,@PhoneNumberOffice,@FeeDescription)";
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+                sqlCommand.Parameters.Add("@TransactionId", System.Data.SqlDbType.VarChar).Value = feeDetails.TransactionId;
+                sqlCommand.Parameters.Add("@Date", System.Data.SqlDbType.VarChar).Value = feeDetails.Date;
+                sqlCommand.Parameters.Add("@PayeeName", System.Data.SqlDbType.DateTime).Value = feeDetails.PayeeName;
+                sqlCommand.Parameters.Add("@FathersName", System.Data.SqlDbType.VarChar).Value = feeDetails.FathersName;
+                sqlCommand.Parameters.Add("@DOB", System.Data.SqlDbType.DateTime).Value = feeDetails.DOB;
+                sqlCommand.Parameters.Add("@PhoneNumberOffice", System.Data.SqlDbType.VarChar).Value = feeDetails.PhoneNumberOffice;
+                sqlCommand.Parameters.Add("@FeeDescription", System.Data.SqlDbType.VarChar).Value = feeDetails.FeeDescription;
+                sqlCommand.Parameters.Add("@Amount", System.Data.SqlDbType.Decimal).Value = feeDetails.Amount;
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+
+            }
+
+        }
+        public void SaveAddressDetails(AddressDetails addressDetails)
+        {
+            string connectionString = System.Configuration.ConfigurationManager.
+            ConnectionStrings["connectionStringName"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "insert into AddressDetails(FullAddress,PhoneNumberOffice,PhoneNumberResidence,EmailId) values(@FullAddress,@PhoneNumberOffice,@PhoneNumberResidence,@EmailId)";
+                SqlCommand sqlCommand = new SqlCommand(query, connection);
+                sqlCommand.Parameters.Add("@FullAddress", System.Data.SqlDbType.NVarChar).Value = addressDetails.FullAddress;
+                sqlCommand.Parameters.Add("@PhoneNumberOffice", System.Data.SqlDbType.VarChar).Value = addressDetails.PhoneNumberOffice;
+                sqlCommand.Parameters.Add("@PhoneNumberResidence", System.Data.SqlDbType.VarChar).Value = addressDetails.PhoneNumberResidence;
+                sqlCommand.Parameters.Add("@EmailId", System.Data.SqlDbType.VarChar).Value = addressDetails.EmailId;
+             
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+
+            }
+
+        }
+    }
+}

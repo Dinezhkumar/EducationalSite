b82557d30fe324b7a9563c5c108203ca45c84d9c
Dinezhkumar <47935348+Dinezhkumar@users.noreply.github.com>
2020-06-03 20:25:45 +0530

    Partial page and controller method

--- EduSite/EduSite/Controllers/HomeController.cs
+++ EduSite/EduSite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static UnivSite.Models.StudentDetails;
 
 namespace EduSite.Controllers
 {
@@ -10,7 +11,44 @@ namespace EduSite.Controllers
     {
         public ActionResult Index()
         {
+            StudentDetailsModel studentDetailsModel = new StudentDetailsModel();
+            return View(studentDetailsModel);
+        }
+
+        public ActionResult SavePersonalDetails()
+        {
             return View();
         }
+        public ActionResult SaveAddressDetails()
+        {
+            return View();
+        }
+        public ActionResult SaveFeeDetails()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertAcademicDetails(AcademicDetails academicDetails)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAcademicDetails(AcademicDetails customer)
+        {
+
+
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAcademicDetails(string CourserId)
+        {
+
+
+            return new EmptyResult();
+        }
+
     }
 }
--- EduSite/EduSite/Models/StudentDetails.cs
+++ EduSite/EduSite/Models/StudentDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,18 @@ namespace UnivSite.Models
 {
     public class StudentDetails
     {
+        public class StudentDetailsModel
+        {
+            public PersonalDetails PersonalDetails { get; set; }
+            public IEnumerable<AcademicDetails> AcademicDetails { get; set; }
+            public AddressDetails AddressDetails { get; set; }
+            public FeeDetails FeeDetails { get; set; }
+
+        }
+
         public class PersonalDetails
         {
+            public string StudentId { get; set; }
             public string ApplicantName { get; set; }
             public string FatherName { get; set; }
             public string MotherName { get; set; }
@@ -18,19 +29,40 @@ namespace UnivSite.Models
         }
         public class AcademicDetails
         {
-            public string CourseName { get; set; }
-            public string FatherName { get; set; }
-            public string MotherName { get; set; }
-            public string RegistrationNo { get; set; }
-            public string NatureOfDocument { get; set; }
-            public string ReasonOfApplying { get; set; }
+            public string AcademicDetailsId { get; set; }
+            public string StudentId { get; set; }
+            public string Course { get; set; }
+            public string Semester { get; set; }
+            public string Year { get; set; }
+            public string CollegeName { get; set; }
+            public string MarksScored { get; set; }
+            public string MaxMark { get; set; }
+            public string UploadFile { get; set; }
+
         }
         public class AddressDetails
         {
-            public string Address { get; set; }
+            public string AddressDetailsDetailsId { get; set; }
+            public string StudentId { get; set; }
+            public string FullAddress { get; set; }
             public string PhoneNumberOffice { get; set; }
             public string PhoneNumberResidence { get; set; }
             public string EmailId { get; set; }
+        }
+        public class FeeDetails
+        {
+            public string FeeDetailsId { get; set; }
+            public string StudentId { get; set; }
+            public string TransactionId { get; set; }
+            public DateTime Date { get; set; }
+            public String PayeeName { get; set; }
+            public string FathersName { get; set; }
+            public DateTime DOB { get; set; }
+            public string PhoneNumberOffice { get; set; }
+            public string FeeCode { get; set; }
+            public string FeeDetail { get; set; }
+            public string FeeDescription { get; set; }
+            public decimal Amount { get; set; }
         }
     }
 }
